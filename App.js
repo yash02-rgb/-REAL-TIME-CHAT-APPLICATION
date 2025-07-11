2f484b532668601349cb6e770a298d489341a032
Yash Dagar <dagaryash700@gmail.com>
2025-07-11 17:11:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import ChatBox from './ChatBox';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Real-Time Chat App</h1>
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
